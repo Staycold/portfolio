937d947b8245cae8fd23991bcce9b8e7fbc6d24f
patrick brodie <patrickxbrodie@hotmail.com>
2021-07-22 22:51:30 -0400

    added the form

--- src/components/Contactme/Contactme.js
+++ src/components/Contactme/Contactme.js
@@ -1,6 +1,16 @@
 import React from 'react';
+import { Form } from 'react-bootstrap';
+import { Button } from 'react-bootstrap';
+import { Modal } from 'react-bootstrap';
+import { useState } from 'react';
+
 
 function Contactme() {
+
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
 
 return (
 <div class="contact">
@@ -12,7 +22,7 @@ return (
 
 </div>
 
-  <div id='contactLinks'>
+  {/* <div id='contactLinks'>
     <ul className='contactLinks'>                   
         <li className='contactList'>
             <a href="mailto:patrickxbrodie@hotmail.com?subject=Portfolio Intrest">Patrickxbrodie@hotmail.com</a>
@@ -28,7 +38,73 @@ return (
         </li>
     </ul>
   </div>
+</div> */}
+
+
+
+{/* <Form>
+<Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+    <Form.Label>Name</Form.Label>
+    <Form.Control type="name" placeholder="name" />
+  </Form.Group>
+  <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+    <Form.Label>Email address</Form.Label>
+    <Form.Control type="email" placeholder="name@example.com" />
+  </Form.Group>
+  <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+    <Form.Label></Form.Label>
+    <Form.Control as="textarea" rows={3} />
+  </Form.Group>
+  <Button variant="primary" type="submit">
+    Submit
+  </Button>
+</Form> */}
+
+
+<>
+      <Button variant="primary" onClick={handleShow}>
+        Contact Me
+      </Button>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Contact Me</Modal.Title>
+        </Modal.Header>
+        <Modal.Body><Form>
+<Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+    <Form.Label>Name</Form.Label>
+    <Form.Control type="name" placeholder="name" />
+  </Form.Group>
+  <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+    <Form.Label>Email address</Form.Label>
+    <Form.Control type="email" placeholder="name@example.com" />
+  </Form.Group>
+  <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+    <Form.Label>Subject</Form.Label>
+    <Form.Control as="textarea" rows={3} />
+  </Form.Group>
+  {/* <Button variant="primary" type="submit">
+    Submit
+  </Button> */}
+</Form></Modal.Body>
+        <Modal.Footer>
+          {/* <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button> */}
+          <Button variant="primary" onClick={handleClose}>
+            Send
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+
 </div>
+
+
+
+
+
+
 )}
 
 export default Contactme
--- src/components/Work/NavTabs.js
+++ src/components/Work/NavTabs.js
@@ -1,6 +1,5 @@
 import React from 'react';
 
-// TODO: Add a comment explaining how we are able to extract the key value pairs from props
 
 function NavTabs({ currentProject, handleProjectChange }) {
   return (
@@ -9,10 +8,9 @@ function NavTabs({ currentProject, handleProjectChange }) {
         <a
           href="#Github"
           onClick={() => handleProjectChange('Github')}
-          //*  TODO: BONUS: Add a comment explaining what kind of operator this is and what it is checking for
-
+          
           className={currentProject === 'Github' ? 'nav-link active' : 'nav-link'}
-        >
+          id='nav-item'>
            Github
         </a>
       </li>
@@ -20,10 +18,9 @@ function NavTabs({ currentProject, handleProjectChange }) {
         <a
           href="#Esports-Dashboard"
           onClick={() => handleProjectChange('Esports Dashboard')}
-          //  TODO: Add a comment explaining what this logic is doing
-
+          
           className={currentProject === 'Esports Dashboard' ? 'nav-link active' : 'nav-link'}
-        >
+         id='nav-item'>
            Esports Dashboard
         </a>
       </li>
@@ -31,32 +28,29 @@ function NavTabs({ currentProject, handleProjectChange }) {
         <a
           href="#Plan-Your-Trip"
           onClick={() => handleProjectChange('Plan Your Trip')}
-          //  TODO: Add a comment explaining what this logic is doing
-
+         
           className={currentProject === 'Plan Your Trip' ? 'nav-link active' : 'nav-link'}
-        >
+          id='nav-item'>
            Plan Your Trip
         </a>
       </li>
       <li className="nav-item">
         <a
           href="#The-Tech-Blog"
-          //  TODO: Add a comment explaining what this logic is doing
-
+          
           onClick={() => handleProjectChange('The Tech Blog')}
           className={currentProject === 'The Tech Blog' ? 'nav-link active' : 'nav-link'}
-        >
+          id='nav-item'>
           The Tech Blog
         </a>
       </li>
       <li className="nav-item">
         <a
           href="#Fitness-Tracker"
-          //  TODO: Add a comment explaining what this logic is doing
-
+         
           onClick={() => handleProjectChange('Fitness Tracker')}
           className={currentProject === 'Fitness Tracker' ? 'nav-link active' : 'nav-link'}
-        >
+          id='nav-item'>
           Fitness Tracker
         </a>
       </li>
